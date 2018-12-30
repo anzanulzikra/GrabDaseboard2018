3d1542ad6b3679c1b3d5ab94a9dfecb69034e6a0
Your Name <zikradroid@gmai.com>
2018-12-30 08:41:25 +0800

    first commit

---
+++ app/src/main/java/com/example/zikra/grabdasboard/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.zikra.grabdasboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.example.zikra.grabdasboard.R.layout.activity_main);
+    }
+}
